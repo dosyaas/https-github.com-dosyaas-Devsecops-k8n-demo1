ba92f8567cb7bd22b72015209107fb4fc89a7286
dosyaas <eldos.sailaukhanov@gmail.com>
2025-10-18 15:05:12 +0500

    ad

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -2,53 +2,67 @@ package com.devsecops;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Простейший REST-контроллер:
+ *  GET /                      -> "Kubernetes DevSecOps"
+ *  GET /compare/{value}       -> текст сравнения с 50
+ *  GET /increment/{value}     -> проксирует в NodeJS-сервис /plusone и возвращает число
+ */
 @RestController
 public class NumericController {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
-	
-	RestTemplate restTemplate = new RestTemplate();
-	
-	@RestController
-	public class compare {
+    private static final Logger logger = LoggerFactory.getLogger(NumericController.class);
 
-		@GetMapping("/")
-		public String welcome() {
-			return "Kubernetes DevSecOps";
-		}
+    // Можно задавать через переменную окружения NODE_SERVICE_URL
+    // или application.properties: node.service.url=http://node-service:5000/plusone
+    @Value("${node.service.url:http://node-service:5000/plusone}")
+    private String baseURL;
 
-		@GetMapping("/compare/{value}")
-		public String compareToFifty(@PathVariable int value) {
-			String message = "Could not determine comparison";
-			if (value > 50) {
-				message = "Greater than 50";
-			} else {
-				message = "Smaller than or equal to 50";
-			}
-			return message;
-		}
+    private final RestTemplate restTemplate;
 
-		@GetMapping("/increment/{value}")
-		public int increment(@PathVariable int value) {
-			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
-			String response = responseEntity.getBody();
-			logger.info("Value Received in Request - " + value);
-			logger.info("Node Service Response - " + response);
-			return Integer.parseInt(response);
-		}
-	}
+    public NumericController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Kubernetes DevSecOps";
+    }
+
+    @GetMapping("/compare/{value}")
+    public String compareToFifty(@PathVariable int value) {
+        if (value > 50) {
+            return "Greater than 50";
+        } else {
+            return "Smaller than or equal to 50";
+        }
+    }
+
+    @GetMapping("/increment/{value}")
+    public int increment(@PathVariable int value) {
+        String url = baseURL + "/" + value;
+        logger.info("Calling Node service: {}", url);
+
+        ResponseEntity<String> responseEntity =
+                restTemplate.getForEntity(url, String.class);
+
+        String body = responseEntity.getBody();
+        logger.info("Value Received in Request - {}", value);
+        logger.info("Node Service Response - {}", body);
+
+        return Integer.parseInt(body);
+    }
 }
